0307591237dcdee4c061c904ce67ac32c4dbbcdc
katja4erry <106451546+katja4erry@users.noreply.github.com>
2022-09-24 14:57:40 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by Ekateryna Myhalchenko and is{" "}
-        <a
-          href="https://github.com/katja4erry/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by Ekateryna Myhalchenko and is{" "}
+          <a
+            href="https://github.com/katja4erry/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
