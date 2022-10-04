e74295487147f9c5a521a2dcf54e7e2cfa1546b5
katja4erry <106451546+katja4erry@users.noreply.github.com>
2022-10-04 16:06:28 +0200

    forecast Layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="row">
             <div className="col-4 pe-0 ps-4 pt-1">
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <div className="col-8 ps-0">
               <WeatherTemperature celsius={props.data.temperature} />
